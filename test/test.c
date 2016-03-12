1ce2280a8e25f696fa36e0fd8c48e9a62f2aadba
nareo2 <kreakil@naver.com>
2016-03-12 17:03:36 +0900

    작동한다면 커널 코딩 스타일에 따라 수정해야함

--- test/test.c
+++ test/test.c
@@ -1,64 +1,76 @@
 #include <stdio.h>
 #include <linux/unistd.h>
 #include <linux/prinfo.h>
-#define __NR_helloworld 382
+#define __NR_ptree 382
 #define no_child 0
 #define have_child 1
+
+typedef struct prinfo {
+        int state;
+        int pid;
+        int parent_pid;
+        int first_child_pid;
+        int next_sibling_pid;
+        int uid;
+        char comm[64];
+};
+
 int main()
 {
-	int nr = 50;
-	prinfo buf[nr]; 
-	syscall(__NR_helloworld, &buf, &nr);
+	int nr = 50;			//randomly choosed
+	struct prinfo buf[nr]; 
+	syscall(__NR_ptree, &buf, &nr);
 	dfs(buf , &nr);
 	return 0;
 }
 
-void dfs(prinfo *buf, int *nr)
+void dfs(struct prinfo *buf, int *nr)
 {
-	prinfo node = buf[0];
-	int index = 0;   
-	int indent_index = 0;   
-	int parent_arr[*nr];    
+        int index = 0;
+        int indent_index = 0;
+        int parent_arr[*nr];
 
-	while(1)
-		indent_print(indent_index);
-		info_print(&node);
-		index++;
-		//indet_index = indent_cal(&node, indent_index);
-		if(!node.fisrt_child_pid){ 		
-			if(!node.next_sibling_pid)	
-				parent_arr[indent_index]=no_child; //이거 영어로 정의할것
-			else			
-				parent_arr[indent_index]=have_child;
-			indent_index++;
-		}else{
-			while(!node.next_sibling_pid){
-				if(node.pid)
-					return 
-				indent_index = indent_cal(parrent_arr,indent_index);
-			}
-			
-		}
-	}
+        while(1){
+                struct prinfo node = buf[index];
+                indent_print(indent_index);
+                info_print(&node);
+                if(node.first_child_pid){
+                        if(buf[index+1].next_sibling_pid)
+                                parent_arr[indent_index]=have_child;
+                        else
+                                parent_arr[indent_index]=no_child;
+                        indent_index++;
+                }else{
+                        if(!node.next_sibling_pid){
+                                if(node.pid==0)
+                                        return
+                                indent_index = indent_cal(parent_arr,indent_index);
+                        }
+                }
+                index++;
+        }
 }
+
 void indent_print(int index)
 {
-	int i = 0;
-	for(; i < index; i++)
-		printf("\t");
+        int i = 0;
+        for(; i < index; i++)
+                printf("\t");
 }
-void info_print(prinfo *node)
+void info_print(struct prinfo *node)
 {
-	printf("%s, %d, %d, %d, %d, %d, %d\n", node->comm, node->state, 
-                        node->pid, node->parent_pid. node->first_child_pid,
+        printf("%s, %d, %d, %d, %d, %d, %d\n", node->comm, node->state,
+                        node->pid, node->parent_pid, node->first_child_pid,
                         node->next_sibling_pid, node->uid);
 }
 
 int indent_cal(int *arr , int indent_index)
 {
-	while(!arr[indent_index])
-		indent_index--;
-	return indent_index;	
+        while(!arr[indent_index])
+                if(indent_index ==0)
+                        return 0;
+                indent_index--;
+        return indent_index;
 }
 
 
