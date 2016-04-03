8e287afaeb5da56fc012b9d13487e1ddd1836c70
bellatoris <bellatoris@snu.ac.kr>
2016-04-03 17:10:59 +0900

    extern 선언으로 해결

--- include/linux/rotation.h
+++ include/linux/rotation.h
@@ -10,11 +10,32 @@
 
 struct dev_rotation {
         int degree;     /* 0 <= degree < 360 */
-}; 
+};
+/*
+ * Defines rotation range. The ranges give the "width" of
+ * the range. If one of the range values is 0, it is not considered
+ * as defining the range (ignored).
+ */
+struct rotation_range {
+        struct dev_rotation rot;  /* device rotation */
+	unsigned int degree_range;      /* lock range = rot.degree ± degree_range */
+};
+
+struct dev_rotation rotation;
+EXPORT_SYMBOL(rotation);
 
 asmlinkage int sys_set_rotation(struct dev_rotation __user *rot);
 
-asmlinkage int sys_get_rotation(struct dev_rotation __user *rot);
+/* Take a read/or write lock using the given rotation range
+ * returning 0 on success, -1 on failure.
+ * system call numbers 385 and 386
+ */
+asmlinkage int sys_rotlock_read(struct rotation_range __user *rot);
+asmlinkage int sys_rotlock_write(struct rotation_range __user *rot);
 
-asmlinkage int sys_rot_lock_read();
-asmlinkage int sys_rot_lock_write();
+/* Release a read/or write lock using the given rotation range
+ * returning 0 on success, -1 on failure.
+ * system call numbers 387 and 388 
+ */
+asmlinkage int sys_rotunlock_read(struct rotation_range __user *rot);
+asmlinkage int sys_rotunlock_write(struct rotation_range __user *rot);
--- kernel/rotation.c
+++ kernel/rotation.c
@@ -1,25 +1,29 @@
 #include <linux/rotation.h>
 
-struct dev_rotation rotation;
-EXPORT_SYMBOL(rotation);
+extern struct dev_rotation rotation;
 
 asmlinkage int sys_set_rotation(struct dev_rotation __user *rot) 
 {
 	get_user(rotation.degree, &rot->degree);
+	printk("%d\n", rotation.degree);
 	return 0;
 }
 
-asmlinkage int sys_get_rotation(struct dev_rotation __user *rot)
-{
-	put_user(rotation.degree, &rot->degree);
-	return 0;
-};
-
-asmlinkage int sys_rot_lock_read()
+asmlinkage int sys_rotlock_read(struct rotation_range __user *rot)
 {
 	return 0;
 }
-asmlinkage int sys_rot_lock_write()
+asmlinkage int sys_rotlock_write(struct rotation_range __user *rot)
+{
+	return 0;
+}
+
+asmlinkage int sys_rotunlock_read(struct rotation_range __user *rot)
+{
+	return 0;
+}
+
+asmlinkage int sys_rotunlock_write(struct rotation_range __user *rot)
 {
 	return 0;
 }
