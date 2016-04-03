5138c29a2bd52e1357a390bab9e84d2580c2f528
bellatoris <bellatoris@snu.ac.kr>
2016-04-03 17:11:59 +0900

    pull request를 위한 commit

--- kernel/rotation.c
+++ kernel/rotation.c
@@ -5,7 +5,6 @@ extern struct dev_rotation rotation;
 asmlinkage int sys_set_rotation(struct dev_rotation __user *rot) 
 {
 	get_user(rotation.degree, &rot->degree);
-	printk("%d\n", rotation.degree);
 	return 0;
 }
 
