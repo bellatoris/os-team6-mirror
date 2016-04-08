03996015bc03c4a4b1e42fe306f17ea6550a083a
bellatoris <bellatoris@snu.ac.kr>
2016-04-08 22:45:41 +0900

    test 완료

--- test/trial.c
+++ test/trial.c
@@ -24,16 +24,16 @@ int main(int argc, char* argv[]){
 	int n;
 	range.rot.degree = atoi(argv[1]);
 	range.degree_range = atoi(argv[2]);
-	while(1){
+	do {
 	syscall(__NR_rotlock_read, &range);
 	FILE* ff = fopen("integer", "r");
 	fscanf(ff, "%d", &n);
 	fclose(ff);
 	printf("trial : ");
 	print_prime(n);
+	usleep(atoi(argv[3]) * 1000000);
 	syscall(__NR_rotunlock_read, &range);
-	sleep(1);
-	}
+	} while(0);
 }
 
 void print_prime(int n){
@@ -45,7 +45,7 @@ void print_prime(int n){
 	for(i =2; i < n ; i++){
 		if( (n % i) ==0){
 			printf("%d *",i);
-			print_prime( n /i);
+			print_prime( n / i);
 			return; 
 		}
 	}
