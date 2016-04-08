0df13192f28ca93efdd9cfb91360867ba89cd321
nareo2 <kreakil@naver.com>
2016-04-08 21:49:12 +0900

    trial 수정

--- test/trial.c
+++ test/trial.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <linux/unistd.h>
 #include <math.h>
+#include <unistd.h>
 #define __NR_set_rotation 384
 #define __NR_rotlock_read 385
 #define __NR_rotlock_write 386
 #define __NR_rotunlock_read 387
 #define __NR_rotunlock_write 388
+int prime[10000];
+
 
 struct dev_rotation{
 	int degree;
@@ -22,6 +25,24 @@ struct rotation_range{
 int main(int argc, char* argv[]){
 	struct rotation_range range;
 	int n;
+	int i,j;
+	int obj;
+	prime[0]=2;
+	for(i=0;i<10000;i++){
+	obj = prime[i]+1;
+		for(j=0;j<=i;j++){
+			if(obj%prime[j]==0){
+				obj++;
+				j=-1;
+				continue;
+			}
+			else if(i == j ){
+				prime[i+1] = obj;
+				break;
+			}
+				
+		}
+	}
 	range.rot.degree = atoi(argv[1]);
 	range.degree_range = atoi(argv[2]);
 	while(1){
@@ -31,23 +52,26 @@ int main(int argc, char* argv[]){
 	fclose(ff);
 	printf("trial : ");
 	print_prime(n);
+	sleep(argv[2]);
 	syscall(__NR_rotunlock_read, &range);
-	sleep(1);
+	usleep(1000);
 	}
 }
 
 void print_prime(int n){
-	int i = 0;
+	int i;
 	if(n <2){
-		printf("%d\n", n);
 		return;
 	}
-	for(i =2; i < n ; i++){
-		if( (n % i) ==0){
-			printf("%d *",i);
-			print_prime( n /i);
-			return; 
+	for(i =0; i < 10000 ; i++){
+		if(n == prime[i]){
+			printf("%d \n" ,prime[i]);	
+			break;
 		}
-	}
-	printf("%d\n",n);    
+		if( (n % prime[i]) ==0){
+			printf("%d * ",prime[i]);
+			n = (n / prime[i]);
+			i--; 
+		}
+	} 
 }
