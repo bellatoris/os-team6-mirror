5fc45ece9f0cf481120700ae5a6bc7f64722e550
nareo2 <kreakil@naver.com>
2016-04-14 20:09:25 +0900

    trial 변경

--- test/trial.c
+++ test/trial.c
@@ -7,7 +7,7 @@
 #define __NR_rotlock_write 386
 #define __NR_rotunlock_read 387
 #define __NR_rotunlock_write 388
-int prime[100000];
+int prime[10000];
 
 
 struct dev_rotation{
@@ -29,7 +29,7 @@ int main(int argc, char* argv[]){
 	int i,j;
 	int obj;
 	prime[0]=2;
-	for(i=0;i< 100000;i++){
+	for(i=0;i< 10000;i++){
 	obj = prime[i]+1;
 		for(j=0;j<=i;j++){
 			if(obj%prime[j]==0){
@@ -62,7 +62,7 @@ void print_prime(int n){
 	if(n <2){
 		return;
 	}
-	for(i =0; i < 100000 ; i++){
+	for(i =0; i < 10000 ; i++){
 		if(n == prime[i]){
 			printf("%d \n" ,prime[i]);	
 			break;
