d6c2b67ed5cd10c32027bcc57914aabbc2ff1631
sangmoon <cholsoo22001@gmail.com>
2016-05-10 19:10:10 +0900

    add proto functions for wrr_sched_class

--- kernel/sched/wrr.c
+++ kernel/sched/wrr.c
@@ -1,43 +1,50 @@
-static void
-enqueue_task_wrr(struct rq *rq, struct task_struct *p, int flags){
+static void enqueue_task_wrr (struct rq *rq, struct task_struct *p, int flags);
+static void dequeue_task_wrr (struct rq *rq, struct task_struct *p, int flags);
+static void yield_task_wrr (struct rq *rq);
+static bool yield_to_task_wrr (struct rq *rq, struct task_struct *p, bool preempt); // bool possible?
 
-}
+static void check_preempt_curr_wrr (struct rq *rq, struct task_struct *p, int flags);
 
-static void
-dequeue_task_wrr(struct rq *rq, struct task_struct *p, int flags){
+static struct task_struct * pick_next_task_wrr (struct rq *rq);
+static void put_prev_task_wrr (struct rq *rq, struct task_struct *p);
 
-}
+#ifdef CONFIG_SMP
+static int  select_task_rq_wrr(struct task_struct *p, int sd_flag, int flags);
+static void migrate_task_rq_wrr(struct task_struct *p, int next_cpu);
 
-static void yield_task_wrr(struct rq *rq){
+static void pre_schedule_wrr (struct rq *this_rq, struct task_struct *task);
+static void post_schedule_wrr (struct rq *this_rq);
+static void task_waking_wrr (struct task_struct *task);
+static void task_woken_wrr (struct rq *this_rq, struct task_struct *task);
 
-}
+static void set_cpus_allowed_wrr (struct task_struct *p,
+				 const struct cpumask *newmask);
 
-static void
-check_preempt_curr_wrr(struct rq *rq, struct task_struct *p){
+static void rq_online_wrr(struct rq *rq);
+static void rq_offline_wrr(struct rq *rq);
+#endif
 
-}
+static void set_curr_task_wrr (struct rq *rq);
+static void task_tick_wrr (struct rq *rq, struct task_struct *p, int queued);
+static void task_fork_wrr (struct task_struct *p);
 
-static struct task_struct*
-pick_next_task_wrr(struct rq *rq){
+static void switched_from_wrr (struct rq *this_rq, struct task_struct *task);
+static void switched_to_wrr (struct rq *this_rq, struct task_struct *task);
+static void prio_changed_wrr (struct rq *this_rq, struct task_struct *task,
+			     int oldprio);
 
-}
+static unsigned int get_rr_interval_wrr (struct rq *rq,
+					 struct task_struct *task);
 
-static void
-put_prev_task_wrr(struct rq *rq, struct task_struct *p){
 
-}
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	void task_move_group_wrr (struct task_struct *p, int on_rq);
+#endif
+};
 
-static unsigned long
-load_balance_wrr(struct rq *this_rq, int this_cpu,
-	struct rq *busiest,
-	unsigned long max_nr_move, unsigned long max_load_move,
-	struct sched_domain *sd, enum cpu_idle_type idle,
-	int *all_pinned, int *this_best_prio){
-
-}
 
 const struct sched_class wrr_sched_class = {
-	.next			= &fair_sched_class, //?? is this mean a priority??
+	.next			= &fair_sched_class,
 	.enqueue_task		= enqueue_task_wrr,
 	.dequeue_task		= dequeue_task_wrr,
 	.yield_task		= yield_task_wrr,
