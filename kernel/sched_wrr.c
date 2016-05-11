e798f5348b0d26ba8d06197ce156f53c09726fbc
sangmoon <cholsoo22001@gmail.com>
2016-05-11 16:18:45 +0900

    added comments at syscall

--- kernel/sched_wrr.c
+++ kernel/sched_wrr.c
@@ -5,11 +5,18 @@ asmlinkage int sys_sched_getweight(pid_t pid, int weight);
 asmlinkage int sys_dummy1(void);
 asmlinkage int sys_dummy2(void);
 
-
+/* Set the SCHED_WRR weight of process, as identified by 'pid'.
+ * If 'pid' is 0, set the weight for the calling process.
+ * System call number 384.
+ */
 asmlinkage int sys_sched_setweight(pid_t pid, int weight){
 	return 0;
 }
 
+/* Obtain the SCHED_WRR weight of a process as identified by 'pid'.
+ * If 'pid' is 0, return the weight of the calling process.
+ * System call number 385.
+ */
 asmlinkage int sys_sched_getweight(pid_t pid, int weight){
 	return 0;
 }
