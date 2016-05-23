7c7d1e25120a4141c8b25af3bbc8da2cdb998266
bellatoris <bellatoris@snu.ac.kr>
2016-05-23 17:00:53 +0900

    normalize 변경

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3907,10 +3907,11 @@ __setscheduler(struct rq *rq, struct task_struct *p, int policy, int prio)
 				do_set_cpus_allowed(p, &hmp_slow_cpu_mask);
 			}
 #endif
-	} else if (policy == SCHED_WRR)
+	} else if (policy == SCHED_WRR) {
 		p->sched_class = &wrr_sched_class;
-	else
+	} else {
 		p->sched_class = &fair_sched_class;
+	}
 
 	set_load_weight(p);
 }
@@ -4646,7 +4647,6 @@ SYSCALL_DEFINE1(sched_get_priority_max, int, policy)
 	case SCHED_BATCH:
 	case SCHED_IDLE:
 	case SCHED_WRR:
-
 		ret = 0;
 		break;
 	}
@@ -7237,7 +7237,7 @@ static void normalize_task(struct rq *rq, struct task_struct *p)
 	on_rq = p->on_rq;
 	if (on_rq)
 		dequeue_task(rq, p, 0);
-	__setscheduler(rq, p, SCHED_NORMAL, 0);
+	__setscheduler(rq, p, SCHED_WRR, 0);
 	if (on_rq) {
 		enqueue_task(rq, p, 0);
 		resched_task(rq->curr);
