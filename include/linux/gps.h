ca4dc65a6a0ddbd0fd81ae4474413197128f662e
sangmoon <cholsoo22001@gmail.com>
2016-06-10 19:32:19 +0900

    시스콜 프로토 타입 추가

--- include/linux/gps.h
+++ include/linux/gps.h
@@ -1,5 +1,15 @@
+#include <linux/export.h>
+
 struct gps_location {
 	double latitude;
 	double longitude;
 	float accuracy; /* in meters */
 };
+
+/*
+ * location global variable
+ */
+struct gps_location location;
+EXPORT_SYMBOL(location);
+
+int set_gps_location(struct pgs_location __user *loc);
--- kernel/gps.c
+++ kernel/gps.c
@@ -1,5 +1,10 @@
-#include "linux/gps.h"
+#include <linux/gps.h>
+extern gps_location location;
 
 int set_gps_location(struct pgs_location __user *loc){
+
+}
+
+int get_gps_location(){
 
 }
